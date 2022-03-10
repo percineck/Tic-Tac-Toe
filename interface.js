e6601b7686c2daf9fa4212fc36ebdb18e40663cd
Luis Eduardo Meneghel Percicoti <86788255+percineck@users.noreply.github.com>
2022-03-09 22:47:16 -0300

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let squares = document.querySelectorAll(".square")
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick)
+    })
+})
+
+function handleClick(event) {
+    // console.log(event.target)
+    let square = event.target
+    let position = square.id
+
+    if (handleMove(position)) {
+
+        setTimeout(() =>{
+            alert("O jogo acabou o vencedor foi " + playerTime)
+
+        }, 50)
+    }
+    updateSquare(position)
+}
+
+function updateSquare(position) {
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+
+// function updateSquares() {
+//     let squares = document.querySelectorAll(".square")
+
+//     squares.forEach((square) => {
+
+//         let position = square.id
+
+//         let symbol = board[position]
+
+//         if (symbol != '') {
+//             square.innerHTML = `<div class='${symbol}'></div>`
+//         }
+//     })
+// }
+
